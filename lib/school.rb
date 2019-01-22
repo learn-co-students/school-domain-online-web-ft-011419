e7e5d85ea9ad5cdcdf8b1882cac1f76a1721ad40
Luis Zabala <luis.zabala076@gmail.com>
2019-01-22 19:30:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,10 @@ class School
      @roster = {}
    end
      
+#hash["new_key"] = []
+#hash["new_key"] << "new_value_for_value_array"
+#hash => {"new_key"=>["new_value_for_value_array"]} 
+     
    def add_student(student, grade)
        roster[grade] ||= []
        roster[grade] << student
