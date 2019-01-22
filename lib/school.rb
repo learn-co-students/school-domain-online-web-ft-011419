cf91ef3e60fa83dacb191cc2a6e7ba2bebf2d9ad
Trent Moss <trentmoss05@gmail.com>
2019-01-22 21:15:27 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School
+
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+
+  def roster
+    roster = {}
+  end
+
+  def add_student(name, age)
+    roster[:name] = age
+
+end
