06c3d6ea2144deaffa261a755bb013a16a6f626f
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-23 23:48:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_accessor :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] = [] unless @roster.key?(grade)
+    @roster[grade] << name
+  end
+  
+  def grade(_grade)
+    @roster[_grade]
+  end
+  
+  def sort
+    newhash = {}
+    @roster.each do |grade, names|
+      newhash[grade] = names.sort
+    end
+    newhash
+  end
+end
