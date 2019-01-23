12eeb7e2694f4ccbfa1d3201cf10c21fefb6490e
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-23 17:08:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+  end
+end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort_by!{|x| x}
+    end
+
+  end
+
+
+end# code here!
