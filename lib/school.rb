38b1ff51315df2b684364840b7467d0fe3e19edf
powell956 <powell956@gmail.com>
2021-04-23 13:24:12 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,47 @@
-class School
-  attr_reader :roster
+# class School
+#   attr_reader :roster
+#
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
+#
+#   def add_student(name, grade)
+#     if @roster[grade]
+#       @roster[grade] << name
+#     else
+#       @roster[grade] = []
+#       @roster[grade] << name
+#     end
+#   end
+#
+#   def grade(grade)
+#     @roster[grade]
+#   end
+#
+#   def sort
+#     sorted_roster = {}
+#     @roster.each do |grade, students|
+#       sorted_roster[grade] = students.sort
+#     end
+#     sorted_roster
+#   end
+# end
 
-  def initialize(name)
-    @name = name
+class School
+  attr_reader :school_name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
     @roster = {}
   end
 
-  def add_student(name, grade)
-    if @roster[grade]
-      @roster[grade] << name
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
     else
       @roster[grade] = []
-      @roster[grade] << name
+      @roster[grade] << student_name
     end
   end
 
@@ -21,8 +51,8 @@ class School
 
   def sort
     sorted_roster = {}
-    @roster.each do |grade, students|
-      sorted_roster[grade] = students.sort
+    @roster.each do |grade, student_list|
+      sorted_roster[grade] = student_list.sort
     end
     sorted_roster
   end
